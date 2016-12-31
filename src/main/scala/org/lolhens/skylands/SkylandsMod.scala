116759665dca16103776740892b1cd1a75575b87
Pierre Kisters <pierrekisters@gmail.com>
2016-12-31 01:51:53 +0100

    added files

---
+++ src/main/scala/org/lolhens/skylands/SkylandsMod.scala
@@ -0,0 +1,23 @@
+package org.lolhens.skylands
+
+import net.minecraft.init.Blocks
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+
+/**
+  * Created by pierr on 31.12.2016.
+  */
+@Mod(modid = SkylandsMod.ModId, version = SkylandsMod.Version)
+class SkylandsMod {
+  @EventHandler
+  def init(event: FMLInitializationEvent) {
+    println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName)
+  }
+}
+
+object SkylandsMod {
+  val ModId = "skylandsmod"
+
+  val Version = "0.0.0"
+}
